18214a12b27e79d4da27c69413169ab1ae2f2405
Marcus Franco <rasfmar@rasfmar-2.local>
2020-05-28 11:58:22 -0400

    Fixed navbar, mobile compliancy

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -16,9 +16,9 @@ export class ProjectsComponent implements OnInit {
       {
         name: 'Compact',
         icon: '/assets/img/Angular.png',
-        desc: 'Compact was my attempt at creating my own programming language using C++. I created a basic lexical analyzer and token parser. I had to refine my understanding of pointers, memory allocation, and garbage collection to optimize the interpreter.',
+        desc: 'Compact is my attempt at creating my own programming language using C++. I created a basic lexical analyzer and token parser. I had to refine my understanding of pointers, memory allocation, and garbage collection to optimize the interpreter.',
         links: [
-          ['GitHub','https://github.com/rasfmar/Compact']
+          ['GitHub (WIP)','https://github.com/rasfmar/Compact']
         ]
       },
       {
@@ -26,16 +26,24 @@ export class ProjectsComponent implements OnInit {
         icon: '/assets/img/Angular.png',
         desc: 'AngularSandbox is a web application developed in Angular to practice its full capabilities. You’ll find several different components with random functions.',
         links: [
-          ['Link','#'],
-          ['GitHub','https://github.com/rasfmar/Compact']
+          ['Link (HTTP)','http://35.155.41.181:1026/'],
+          ['GitHub (WIP)','https://github.com/rasfmar/AngularSandbox']
         ]
       },
       {
-        name: 'RasfOS',
+        name: 'Small',
         icon: '/assets/img/Angular.png',
-        desc: 'RasfOS is an operating system I built in the summer of 2019 exploring x86 Intel assembly to construct a basic operating system.',
+        desc: 'Small is a virtual machine I will begin working on in C++. It will feature its own allocated memory, registers, flags, and machine language utilizing an emulated CPU and ALU\'s operations and software interrupts with I/O access.',
         links: [
-          ['GitHub','https://github.com/rasfmar/Compact']
+          ['GitHub (WIP)','https://github.com/rasfmar/Small']
+        ]
+      },
+      {
+        name: 'DrawingOS',
+        icon: '/assets/img/Angular.png',
+        desc: 'DrawingOS is an "operating system" I created with x86 Intel assembly and compiled with nasm. It is intended for DOS and only features a drawing application which can be controlled with the keyboard.',
+        links: [
+          ['GitHub (not updated)','https://github.com/rasfmar/RasfOS']
         ]
       },
     ];
