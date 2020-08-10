a19aa334d01ebb25e7a4269f90e29cf31cd74c06
Marcus Franco <rasfmar@rasfmar-2.local>
2020-08-10 08:25:47 -0400

    gatsby boilerplate

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Home() {
+  return <div>you've reached marcus franco. my site is down so all you gotta know is that i'm a dev who can be reached at <a href="mailto:m6franco@ucsd.edu">m6franco@ucsd.edu</a></div>
+}
